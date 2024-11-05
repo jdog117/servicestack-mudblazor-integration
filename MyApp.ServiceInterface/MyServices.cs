62b2a48bdc0e09abac01f0231cde95ac48d14409
Joseph Casey <jspcasey@gmail.com>
2024-11-05 10:48:28 -0800

    Initial commit

---
+++ MyApp.ServiceInterface/MyServices.cs
@@ -0,0 +1,12 @@
+using ServiceStack;
+using MyApp.ServiceModel;
+
+namespace MyApp.ServiceInterface;
+
+public class MyServices : Service
+{
+    public object Any(Hello request)
+    {
+        return new HelloResponse { Result = $"Hello, {request.Name}!" };
+    }
+}
